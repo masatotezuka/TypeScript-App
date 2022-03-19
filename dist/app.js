f96a1e2d734ec0da5d7139e5dae09351f40f4129
masatotezuka <tez.0731.tez@gmail.com>
2022-03-19 16:21:25 +0900

    section5-interface

--- dist/app.js
+++ dist/app.js
@@ -1,119 +1,38 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Department = /** @class */ (function () {
-    function Department(id, name) {
-        this.id = id;
+// type AddFun = (a: number, b: number) => number;
+var add;
+add = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(add(1, 2));
+var user1;
+user1 = {
+    name: "Mike",
+    age: 23,
+    greet: function (text) {
+        console.log("".concat(text, ". My name is ").concat(this.name));
+    },
+};
+user1.greet("Hello!");
+//interfaceはマージできる。
+// interface Greetable extends Named, AnotherInterface {
+//   greet(text: string): void;
+// }
+var Human = /** @class */ (function () {
+    function Human(name) {
+        this.age = 30;
         this.name = name;
-        // private id: string;
-        // name: string;
-        //publicはデフォルト。JsにはPublicとPrivateのの概念はない。
-        this.employees = [];
-        // this.id = id;
-        // this.name = name;
-        console.log(Department.year);
+        if (name) {
+            this.name = name;
+        }
     }
-    Department.crateEmployee = function (name) {
-        return { name: name };
+    Human.prototype.greet = function (text) {
+        if (this.name) {
+            console.log("".concat(text, ". My name is ").concat(this.name));
+        }
     };
-    Department.prototype.describe = function (year) {
-        console.log("Department(".concat(this.id, ") is ").concat(this.name, ". ").concat(year, " year. "));
-    };
-    Department.prototype.addEmployee = function (employee) {
-        this.employees.push(employee);
-    };
-    Department.prototype.printEmployeeInformation = function () {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    };
-    Department.year = 2020;
-    return Department;
+    return Human;
 }());
-var accounting = new Department("dep1", "Accounting");
-var employee1 = Department.crateEmployee("Jack");
-console.log(employee1, Department.year);
-console.log(accounting);
-accounting.describe(45);
-accounting.addEmployee("Max");
-accounting.addEmployee("Mike");
-accounting.printEmployeeInformation();
-//クラスの継承
-var ITDepartment = /** @class */ (function (_super) {
-    __extends(ITDepartment, _super);
-    function ITDepartment(id, admins) {
-        var _this = _super.call(this, id, "IT") || this;
-        _this.admins = admins;
-        return _this;
-    }
-    return ITDepartment;
-}(Department));
-var ITAccounting = new ITDepartment("d1", ["Max"]);
-console.log(ITAccounting);
-var customerDepartment = /** @class */ (function (_super) {
-    __extends(customerDepartment, _super);
-    function customerDepartment(id, reports) {
-        var _this = _super.call(this, id, "customer") || this;
-        _this.reports = [];
-        _this.lastReport = reports[0];
-        return _this;
-    }
-    Object.defineProperty(customerDepartment.prototype, "mostRecentReport", {
-        get: function () {
-            if (this.lastReport) {
-                return this.lastReport;
-            }
-            throw new Error("レポートが見つかりません。");
-        },
-        set: function (value) {
-            if (!value) {
-                throw new Error("値が正しくありません。");
-            }
-            this.addReport(value);
-        },
-        enumerable: false,
-        configurable: true
-    });
-    customerDepartment.prototype.addReport = function (text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    };
-    customerDepartment.prototype.printResult = function () {
-        console.log(this.reports);
-    };
-    customerDepartment.prototype.addEmployee = function (name) {
-        if (name === "Max") {
-            return;
-        }
-        else {
-            this.employees.push(name);
-        }
-    };
-    return customerDepartment;
-}(Department));
-var customer = new customerDepartment("dep2", []);
-console.log(customer);
-customer.addReport("texttext....");
-customer.mostRecentReport = "レポート";
-console.log(customer.mostRecentReport);
-customer.printResult();
-console.log(customer);
-customer.addEmployee("Max");
-customer.addEmployee("mike");
-customer.printEmployeeInformation();
-//クラスDepartment内でのみアクセス可能
-// accounting.employees[2] = "Anna";
-// const accountingCopy = { name: "Dummy", describe: accounting.describe };
-// accountingCopy.describe(23);
+var user2;
+user2 = new Human("Anna");
+user2.greet("Hello!");
--- src/app.ts
+++ src/app.ts
@@ -1,103 +1,64 @@
-class Department {
-  // private id: string;
-  // name: string;
-  //publicはデフォルト。JsにはPublicとPrivateのの概念はない。
-  protected employees: string[] = [];
-  static year = 2020;
-  static crateEmployee(name: string) {
-    return { name: name };
-  }
-  constructor(private readonly id: string, public name: string) {
-    // this.id = id;
-    // this.name = name;
-    console.log(Department.year);
-  }
-  describe(this: Department, year: number) {
-    console.log(`Department(${this.id}) is ${this.name}. ${year} year. `);
-  }
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+// type AddFun = (a: number, b: number) => number;
+
+interface AddFun {
+  (a: number, b: number): number;
 }
 
-const accounting = new Department("dep1", "Accounting");
-const employee1 = Department.crateEmployee("Jack");
-console.log(employee1, Department.year);
+let add: AddFun;
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+console.log(add(1, 2));
 
-console.log(accounting);
-
-accounting.describe(45);
-
-accounting.addEmployee("Max");
-accounting.addEmployee("Mike");
-accounting.printEmployeeInformation();
-
-//クラスの継承
-class ITDepartment extends Department {
-  admins: string[];
-  constructor(id: string, admins: string[]) {
-    super(id, "IT");
-    this.admins = admins;
-  }
+interface Person {
+  name: string;
+  age: number;
+  greet(text: string): void;
 }
 
-const ITAccounting = new ITDepartment("d1", ["Max"]);
-console.log(ITAccounting);
+let user1: Person;
 
-class customerDepartment extends Department {
-  private reports: string[] = [];
-  private lastReport: string;
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("レポートが見つかりません。");
-  }
+user1 = {
+  name: "Mike",
+  age: 23,
+  greet(text: string) {
+    console.log(`${text}. My name is ${this.name}`);
+  },
+};
 
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("値が正しくありません。");
+user1.greet("Hello!");
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
+  greet(text: string): void;
+}
+
+//interfaceはマージできる。
+// interface Greetable extends Named, AnotherInterface {
+//   greet(text: string): void;
+// }
+
+class Human implements Greetable {
+  name: string;
+  age: number = 30;
+  constructor(name: string) {
+    this.name = name;
+    if (name) {
+      this.name = name;
     }
-    this.addReport(value);
   }
-  constructor(id: string, reports: string[]) {
-    super(id, "customer");
-    this.lastReport = reports[0];
-  }
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-  printResult() {
-    console.log(this.reports);
-  }
-  addEmployee(name: string): void {
-    if (name === "Max") {
-      return;
-    } else {
-      this.employees.push(name);
+  greet(text: string): void {
+    if (this.name) {
+      console.log(`${text}. My name is ${this.name}`);
     }
   }
 }
-const customer = new customerDepartment("dep2", []);
-console.log(customer);
 
-customer.addReport("texttext....");
-customer.mostRecentReport = "レポート";
-console.log(customer.mostRecentReport);
-customer.printResult();
-console.log(customer);
+let user2: Greetable;
 
-customer.addEmployee("Max");
-customer.addEmployee("mike");
-customer.printEmployeeInformation();
-
-//クラスDepartment内でのみアクセス可能
-// accounting.employees[2] = "Anna";
-
-// const accountingCopy = { name: "Dummy", describe: accounting.describe };
-// accountingCopy.describe(23);
+user2 = new Human("Anna");
+user2.greet("Hello!");
